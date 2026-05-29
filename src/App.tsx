3ad5e1d31d1c4dfbe69db8470ca813b2900348ff
Kalvin <kalvin.wu@eskyfun.com>
2026-05-29 11:40:37 +0800

    feat: integrate all calendar components with theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
-function App() {
-  return <div className="app-frame">CC-Day</div>;
+import { ThemeProvider } from "./components/ThemeProvider";
+import { useCalendar } from "./hooks/useCalendar";
+import { DayDetail } from "./components/DayDetail";
+import { CalendarGrid } from "./components/CalendarGrid";
+import { MonthNav } from "./components/MonthNav";
+import { FooterBar } from "./components/FooterBar";
+
+function CalendarApp() {
+  const {
+    today,
+    selectedDate,
+    selectedDayInfo,
+    viewYear,
+    viewMonth,
+    monthGrid,
+    prevMonth,
+    nextMonth,
+    goToToday,
+    selectDate,
+  } = useCalendar();
+
+  return (
+    <div className="app-frame">
+      {selectedDayInfo && <DayDetail day={selectedDayInfo} />}
+      <div className="divider" />
+      <CalendarGrid
+        monthGrid={monthGrid}
+        selectedDate={selectedDate}
+        today={today}
+        onSelectDate={selectDate}
+      />
+      <MonthNav
+        year={viewYear}
+        month={viewMonth}
+        onPrev={prevMonth}
+        onNext={nextMonth}
+      />
+      <div className="divider" />
+      {selectedDayInfo && <FooterBar day={selectedDayInfo} onGoToToday={goToToday} />}
+    </div>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider>
+      <CalendarApp />
+    </ThemeProvider>
+  );
+}
